88f9cf6f4a6d5be45f6f05b4331102a5d39464ae
Douglas Bray <douglas.braydev@gmail.com>
2025-02-03 06:37:16 -0600

    finally got data

--- main.js
+++ main.js
@@ -11,17 +11,31 @@ const app = express();
 const PORT = process.env.port || 3000;
 
 
-try {
-  axios(urls.travelers).then(response => {
-    html = response.data;
-  });
+
+const getHtml = async () => {
+
+  let html = "1";
+
+  try {
+    console.log("Entering try block");
+    
+    html = await axios.get(urls.travelers);
+      console.log("Entering response set");
+      console.log(html.data);
+           return html.data;
+ 
+  }
+  catch (error) {
+    console.log(error);
+  }
+
 }
-catch (error) {
-  console.log(error);
-}
 
 
 
+let info = getHtml();
+
+console.log(info);
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT:${PORT}`);
